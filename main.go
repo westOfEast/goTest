bcf1bf6bd88c3969614673f462de99765f0a34bd
w0620060232@sina.com <wangzhi@wangzhi-MacBook-Pro.local>
2017-11-28 23:38:19 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello word")
+	fmt.Println("hello mytest")
+	// Test()
+	// test()
+}
+
+func test() {
+	fmt.Println("hello mytest")
+}

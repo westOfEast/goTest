cb6dd672cbe909e5682fa7a3ec0cd4db6ec105a3
w0620060232@sina.com <wangzhi@wangzhi-MacBook-Pro.local>
2017-12-02 09:33:37 +0800

    modify

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
-package test
+package main
 
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello word")
-	fmt.Println("hello mytest")
+	// fmt.Println("Hello word")
+	// fmt.Println("hello mytest")
 	// Test()
 	// test()
+	Hello()
 }
 
 func test() {
